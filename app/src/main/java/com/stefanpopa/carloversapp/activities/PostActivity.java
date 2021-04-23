579bc0f214bed99b909d4644cb30231a974a04da
StefanPopa02 <60078395+StefanPopa02@users.noreply.github.com>
2021-04-23 15:46:12 +0300

    PostActivity backend upload to firebase storage

--- app/src/main/java/com/stefanpopa/carloversapp/activities/PostActivity.java
+++ app/src/main/java/com/stefanpopa/carloversapp/activities/PostActivity.java
@@ -1,16 +1,110 @@
 package com.stefanpopa.carloversapp.activities;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.hendraanggrian.appcompat.widget.SocialAutoCompleteTextView;
 import com.stefanpopa.carloversapp.R;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 public class PostActivity extends AppCompatActivity {
+
+    private String imageUrl;
+    private Uri imageUri;
+    private ImageView close;
+    private ImageView imageAdded;
+    private TextView post;
+    SocialAutoCompleteTextView description;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        close = findViewById(R.id.close);
+        imageAdded = findViewById(R.id.imageAdded);
+        post = findViewById(R.id.post);
+        description = findViewById(R.id.description);
+
+        close.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PostActivity.this, WelcomeActivity.class));
+                finish();
+            }
+        });
+
+        post.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
+        CropImage.activity().start(PostActivity.this);
+    }
+
+    private void uploadImage() {
+        ProgressDialog pd = new ProgressDialog(this);
+        pd.setMessage("Uploading...");
+        pd.show();
+
+        if(imageUri != null){
+            StorageReference filePath = FirebaseStorage.getInstance().getReference("Posts").child(System.currentTimeMillis()+"."+getFileExtension(imageUri));
+            StorageTask uploadTask = filePath.putFile(imageUri);
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if(!task.isSuccessful()){
+                        throw task.getException();
+                    }
+                    return filePath.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    Uri downloadUri = task.getResult();
+                    imageUrl = downloadUri.toString();
+
+                    //TODO: Continue with Storing to database the URL post
+                }
+            });
+        }
+    }
+
+    private String getFileExtension(Uri imageUri) {
+        return MimeTypeMap.getSingleton().getExtensionFromMimeType(this.getContentResolver().getType(imageUri));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+
+            imageAdded.setImageURI(imageUri);
+        }else{
+            Toast.makeText(this, "Try Again!", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(PostActivity.this, WelcomeActivity.class));
+            finish();
+        }
     }
 }
